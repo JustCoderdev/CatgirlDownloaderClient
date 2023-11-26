9321955455724135142931c6fc1b39995f5b34f2
JustPCode <107036402+JustCoderdev@users.noreply.github.com>
2023-11-26 15:49:41 +0100

    Initial Commit

---
+++ CatgirlDownloaderClient/CatgirlDownloaderClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatgirlDownloaderClientApp.swift
+//  CatgirlDownloaderClient
+//
+//  Created by Teo Perini on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CatgirlDownloaderClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
